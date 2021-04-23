140fd393b04fbe861b192379f931d1b2f4bc55aa
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-23 15:26:08 -0600

    Add route for forecast action in ForecastsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/forecasts', to: 'forecasts#forecast'
+    end
+  end
 end
+
+# get '/items/:id/merchant', to: 'item_merchant#show', as: 'items_merchant'
