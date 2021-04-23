2dc023cc9c70189f10ee983418defa7d14e07d39
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-23 17:32:10 -0600

    Calls to both map quest and open weather return json data and map quest call has parsed out data

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,16 +1,42 @@
 class Api::V1::ForecastsController < ActionController::API
   def forecast
-    conn = Faraday.new(
-      url: 'http://www.mapquestapi.com',
-      params: {key: 'TTmP4GJnMSEgIJ5sd9JKFdSAqA0ZiviA'}
+    # conn_1 = Faraday.new(
+    #   url: 'http://www.mapquestapi.com',
+    #   params: {key: 'TTmP4GJnMSEgIJ5sd9JKFdSAqA0ZiviA'}
+    # )
+
+    conn_1 = Faraday.new(
+      url: "#{ENV['MAP_QUEST_BASE_URL']}",
+      params: {key: "#{ENV['MAP_QUEST_KEY']}"}
     )
 
-    response = conn.get('geocoding/v1/address') do |req|
+    response_1 = conn_1.get('geocoding/v1/address') do |req|
       req.params['location'] = 'Denver,CO'
     end
 
-    geodata = JSON.parse(response.body, symbolize_names: true)
+    geodata = JSON.parse(response_1.body, symbolize_names: true)
 
-    lat_lng = geodata[:results][0][:locations][0][:latLng]
+    lat = geodata[:results][0][:locations][0][:latLng][:lat]
+    lon = geodata[:results][0][:locations][0][:latLng][:lng]
+
+    conn_2 = Faraday.new(
+      url: "#{ENV['OPEN_WEATHER_BASE_URL']}",
+      params: {appid: "#{ENV['OPEN_WEATHER_KEY']}"}
+    )
+
+    response_2 = conn_2.get('/data/2.5/onecall') do |req|
+      req.params['lat'] = "#{lat}"
+      req.params['lon'] = "#{lon}"
+    end
+
+    weather = JSON.parse(response_2.body, symbolize_names: true)
+
+    require "pry"; binding.pry
   end
 end
+
+
+# conn_2 = Faraday.new(
+#   url: 'https://api.openweathermap.org',
+#   params: {appid: 'b3c5b8f6cfb0dccbc94d46c466cab9ab'}
+# )
