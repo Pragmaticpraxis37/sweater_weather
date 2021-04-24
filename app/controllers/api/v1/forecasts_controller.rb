12e842a695ede82b80b92af1ca8be10c8fa1e743
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-24 10:38:30 -0600

    Refactor service calls and creation of objects out of forecast action

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,86 +2,9 @@ require 'ostruct'
 
 class Api::V1::ForecastsController < ActionController::API
   def forecast
-    location = params[:location]
-
-    conn_1 = Faraday.new(
-      url: "#{ENV['MAP_QUEST_BASE_URL']}",
-      params: {key: "#{ENV['MAP_QUEST_KEY']}"}
-    )
-
-    response_1 = conn_1.get('geocoding/v1/address') do |req|
-      req.params['location'] = "#{location}"
-    end
-
-    geodata = JSON.parse(response_1.body, symbolize_names: true)
-
-    lat = geodata[:results][0][:locations][0][:latLng][:lat]
-    lon = geodata[:results][0][:locations][0][:latLng][:lng]
-
-    conn_2 = Faraday.new(
-      url: "#{ENV['OPEN_WEATHER_BASE_URL']}",
-      params: {appid: "#{ENV['OPEN_WEATHER_KEY']}"}
-    )
-
-    response_2 = conn_2.get('/data/2.5/onecall') do |req|
-      req.params['lat'] = "#{lat}"
-      req.params['lon'] = "#{lon}"
-      req.params['units'] = 'imperial'
-    end
-
-    weather = JSON.parse(response_2.body, symbolize_names: true)
-
-
-
-
-    current_weather_data = {
-                        datetime: Time.at(weather[:current][:dt]),
-                        sunrise: Time.at(weather[:current][:sunrise]),
-                        sunset: Time.at(weather[:current][:sunset]),
-                        temperature: weather[:current][:temp],
-                        feels_like: weather[:current][:feels_like],
-                        humidity: weather[:current][:humidity],
-                        uvi: weather[:current][:uvi],
-                        visibility: weather[:current][:visibility],
-                        conditions: weather[:current][:weather][0][:description],
-                        icon: weather[:current][:weather][0][:icon]
-                      }
-
-
-
-    daily_weather_data = weather[:daily][0..4].map do |day|
-                                    {
-                                      date: Time.at(day[:dt]).strftime("%m-%e-%y"),
-                                      sunrise: Time.at(day[:sunrise]),
-                                      sunset: Time.at(day[:sunset]),
-                                      max_temp: day[:temp][:max],
-                                      min_temp: day[:temp][:min],
-                                      conditions: day[:weather][0][:description],
-                                      icon: day[:weather][0][:icon]
-                                    }
-                        end
-
-
-    hourly_weather_data = weather[:hourly][0..7].map do |hour|
-                    {
-                                    time: Time.at(hour[:dt]).strftime("%H:%M:%S"),
-                                    temp: hour[:temp],
-                                    description: hour[:weather][0][:description],
-                                    icon: hour[:weather][0][:icon]
-                      }
-                  end
-
-
-    forecast = OpenStruct.new({
-                                id: nil,
-                                current_weather: current_weather_data,
-                                daily_weather: daily_weather_data,
-                                hourly_weather: hourly_weather_data
-                              })
-
-
-
-    render json: ForecastSerializer.new(forecast)
+    forecast_data = ForecastsFacade.forecast(params[:location])
+    # require "pry"; binding.pry
+    render json: ForecastSerializer.new(forecast_data)
   end
 end
 
