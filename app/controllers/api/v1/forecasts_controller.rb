35f76225496f42582a96b4f6fef1bfc031fdcd84
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-23 19:11:08 -0600

    Complete current_weather data parasing

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -5,13 +5,15 @@ class Api::V1::ForecastsController < ActionController::API
     #   params: {key: 'TTmP4GJnMSEgIJ5sd9JKFdSAqA0ZiviA'}
     # )
 
+    location = params[:location]
+
     conn_1 = Faraday.new(
       url: "#{ENV['MAP_QUEST_BASE_URL']}",
       params: {key: "#{ENV['MAP_QUEST_KEY']}"}
     )
 
     response_1 = conn_1.get('geocoding/v1/address') do |req|
-      req.params['location'] = 'Denver,CO'
+      req.params['location'] = "#{location}"
     end
 
     geodata = JSON.parse(response_1.body, symbolize_names: true)
@@ -27,12 +29,46 @@ class Api::V1::ForecastsController < ActionController::API
     response_2 = conn_2.get('/data/2.5/onecall') do |req|
       req.params['lat'] = "#{lat}"
       req.params['lon'] = "#{lon}"
+      req.params['units'] = 'imperial'
     end
 
     weather = JSON.parse(response_2.body, symbolize_names: true)
 
+    current_weather = weather[:current]
+
+
+    datetime = Time.at(current_weather[:dt])
+    sunrise = Time.at(current_weather[:sunrise])
+    sunset = Time.at(current_weather[:sunset])
+    temperature = current_weather[:temp]
+    feels_like = current_weather[:feels_like]
+    pressure = current_weather[:pressure]
+    humidity = current_weather[:humidity]
+    uvi = current_weather[:uvi]
+    visibility = current_weather[:visibility]
+    description = current_weather[:weather][0][:description]
+    icon = current_weather[:weather][0][:icon]
+
+    # require "pry"; binding.pry
+    hourly_weather = weather[:hourly][0..7]
+
+    eight_hours = []
+
+
+
+    # require "pry"; binding.pry
+
+
+    hourly_weather.each do |hour|
+      require "pry"; binding.pry
+      eight_hours << [:datetime] = Time.at(hour[:dt])
+      # eight_hours[:temp] = hour[:temp]
+      # eight_hours[:conditions] = hour[:weather][0][:description]
+      # eight_hours[:conditions] = hour[:weather][0][:icon]
+    end
+
     require "pry"; binding.pry
-  end
+
 end
 
 
--- spec/requests/geocoding_spec.rb
+++ spec/requests/geocoding_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe 'Geocoding Requests' do
   describe 'obtains forecasts' do
     it 'can obtain forecast information' do
-      VCR.turn_off!
-      WebMock.allow_net_connect!
-      get api_v1_forecasts_path
+      VCR.use_cassette('Denver,CO') do
+      # VCR.turn_off!
+      # WebMock.allow_net_connect!
+      get api_v1_forecasts_path, params: {location: "Denver,CO"}
 
       # require "pry"; binding.pry
 
       # expect(data).to be_a(OpenStruct)
-      VCR.turn_on!
+      # VCR.turn_on!
+      end 
     end
   end
 end
