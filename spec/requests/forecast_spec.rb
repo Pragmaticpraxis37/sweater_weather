fa64f127ba770aa6b45a2ca0dfa88929ddabcfb9
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-24 08:39:44 -0600

    Rename request test from geodata

---
+++ spec/requests/forecast_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Geocoding Requests' do
+  describe 'obtains forecasts' do
+    it 'can obtain forecast information' do
+      VCR.use_cassette('Denver,CO') do
+      get api_v1_forecasts_path, params: {location: "Denver,CO"}
+
+      require "pry"; binding.pry
+
+      expect(data).to be_a(OpenStruct)
+      end
+    end
+  end
+end
+
+
+
+
+
+
+# VCR.turn_off!
+# WebMock.allow_net_connect!
+# VCR.turn_on!
