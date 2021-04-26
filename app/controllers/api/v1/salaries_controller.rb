4017dd88bc10cc358cd5183872eb92e01a2b887d
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-26 10:01:36 -0600

    Finish calls for weather and create hashes of necessary information and begin working with urban areas call

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -5,9 +5,19 @@ class Api::V1::SalariesController < ActionController::API
     coordinates = CoordinatesService.coordinates(params[:destination])
     weather = ForecastsService.forecast(coordinates)
 
-    city = 
+    destination = { destination: params[:destination]}
 
-    forecast =
+    forecast =  {
+                  forecast: { summary: weather[:current][:weather][0][:description],
+                            temperature: "#{weather[:current][:temp]} F" }
+                }
+
+
+    response = Faraday.get('https://api.teleport.org/api/urban_areas/')
+
+    urban_areas_data = JSON.parse(response.body, symbolize_names: true)
+
+    urban_areas = body[:_links][:"ua:item"]
 
     require "pry"; binding.pry
   end
